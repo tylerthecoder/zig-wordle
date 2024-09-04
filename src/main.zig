fb61ff5b4b3bcc586765e5ff5600c0aea63f8ffb
Tyler Tracy <tylertracy1999@gmail.com>
2024-09-03 21:28:36 -0700

    Prints the entire board

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,24 @@
 const std = @import("std");
 const wordfile = "./words.txt";
 
-fn print_with_marks(stdout: anytype, marks: [5]u8, word: []u8) !void {
+const Guess = struct {
+    word: [5]u8,
+    marks: [5]u8,
+};
+
+fn print_guess(stdout: anytype, guess: Guess) !void {
     const black_text = "\x1b[30m";
     const green_bg = "\x1b[42m";
+    const yellow_bg = "\x1b[43m";
     const reset = "\x1b[0m";
+
     for (0..5) |i| {
-        const word_char = word[i];
-        const mark = marks[i];
-        if (mark == 1) {
+        const word_char = guess.word[i];
+        const mark = guess.marks[i];
+        if (mark == 2) {
             try stdout.print("{s}{s}{u}{s}", .{ black_text, green_bg, word_char, reset });
+        } else if (mark == 1) {
+            try stdout.print("{s}{s}{u}{s}", .{ black_text, yellow_bg, word_char, reset });
         } else {
             try stdout.print("{u}", .{word_char});
         }
@@ -17,16 +26,82 @@ fn print_with_marks(stdout: anytype, marks: [5]u8, word: []u8) !void {
     try stdout.print("\n", .{});
 }
 
+fn print_board(board: std.ArrayList(Guess)) !void {
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    std.debug.print("Printing", .{});
+    // Clear the entire screen
+    try stdout.writeAll("\x1b[2J");
+
+    // Move the cursor to the top-left corner (home position)
+    try stdout.writeAll("\x1b[H");
+
+    for (board.items) |guess| {
+        try print_guess(stdout, guess);
+    }
+    try bw.flush(); // don't forget to flush!
+}
+
+fn make_guess(guess_word: [5]u8, target_word: [5]u8) Guess {
+    var new_marks = [5]u8{ 0, 0, 0, 0, 0 };
+    for (0..5) |j| {
+        const guess_letter = guess_word[j];
+        var right_letter = false;
+        var right_place = false;
+        for (0..5) |k| {
+            const target_letter = target_word[k];
+            if (target_letter == guess_letter) {
+                right_letter = true;
+                if (j == k) {
+                    right_place = true;
+                }
+            }
+        }
+
+        if (right_place) {
+            new_marks[j] = 2;
+        } else if (right_letter) {
+            new_marks[j] = 1;
+        }
+    }
+
+    return Guess{
+        .word = guess_word,
+        .marks = new_marks,
+    };
+}
+
+pub fn get_input(board: std.ArrayList(Guess)) ![5]u8 {
+    while (true) {
+        var input: [5]u8 = undefined;
+        const stdin = std.io.getStdIn().reader();
+        const bytes_read = try stdin.read(&input);
+        const stdout_file = std.io.getStdOut().writer();
+        if (bytes_read != 5 or input[4] == '\n') {
+            try stdout_file.writeAll("Too few characters");
+            try stdin.skipUntilDelimiterOrEof('\n');
+            try print_board(board);
+        } else {
+            try stdin.skipUntilDelimiterOrEof('\n');
+            return input;
+        }
+    }
+}
+
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var board = std.ArrayList(Guess).init(allocator);
+    defer board.deinit();
+
     const file = try std.fs.cwd().openFile(wordfile, .{});
     defer file.close();
 
     const file_size = try file.getEndPos();
-
-    // make allocator for the file
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
 
     const buffer = try allocator.alloc(u8, file_size);
     defer allocator.free(buffer);
@@ -45,67 +120,16 @@ pub fn main() !void {
     }
 
     const random_index = rng.intRangeAtMost(u32, 0, word_count - 1);
-
     const word_loc = random_index * 6;
-
     const word = buffer[word_loc .. word_loc + 5];
+    const word_copied: [5]u8 = word[0..5].*;
 
-    // now I need to display the word to the screen with green and blue characters
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    // Clear the entire screen
-    try stdout.writeAll("\x1b[2J");
-
-    // Move the cursor to the top-left corner (home position)
-    try stdout.writeAll("\x1b[H");
-
-    const marks = [5]u8{ 0, 0, 1, 0, 0 };
-
-    try print_with_marks(stdout, marks, word);
-
-    try bw.flush(); // don't forget to flush!
-
-    var input: [6]u8 = undefined;
-    const stdin = std.io.getStdIn().reader();
-
-    _ = try stdin.readUntilDelimiter(&input, '\n');
-
-    var new_marks = [5]u8{ 0, 0, 0, 0, 0 };
-    for (0..5) |j| {
-        const guess_letter = input[j];
-        var right_letter = false;
-        var right_place = false;
-        for (0..5) |k| {
-            const target_letter = word[k];
-            if (target_letter == guess_letter) {
-                right_letter = true;
-                if (j == k) {
-                    right_place = true;
-                }
-            }
-        }
-
-        if (right_place) {
-            new_marks[j] = 2;
-        } else if (right_letter) {
-            new_marks[j] = 1;
-        }
+    while (true) {
+        const input = try get_input(board);
+        const guess = make_guess(input, word_copied);
+        try board.append(guess);
+        try print_board(board);
     }
-
-    // move up a line
-    try stdout.writeAll("\x1b[1A");
-
-    try stdout.writeAll("\r");
-
-    try print_with_marks(stdout, new_marks, &input);
-
-    try bw.flush();
 }
 
 test "simple test" {
